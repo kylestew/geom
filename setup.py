724c3b7bfc3d3bb76cab08dc25fb675bd26cb086
Kyle Stewart <kylestew@gmail.com>
2022-07-17 13:25:54 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="geom",
     version="0.0.1",
     author="Kyle Stewart",
     author_email="kylestew@gmail.com",
-    packages=["geom"],
+    packages=find_packages(),
     description="A geometry data and functions library",
     long_description=open("README.md").read(),
     install_requires=["scipy", "numpy", "shapely", "iteration_utilities"],
