adb0feb17d842ed6f43985e549276684a06abd59
Kyle Stewart <kylestew@gmail.com>
2022-12-17 13:04:33 -0700

    Add random functions area of geom

---
+++ geom/random/__init__.py
@@ -0,0 +1,1 @@
+from .random_point_in import gaussian_point_in, uniform_point_in
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="geom",
-    version="0.0.20",
+    version="0.0.21",
     author="Kyle Stewart",
     author_email="kylestew@gmail.com",
     packages=find_packages(),
