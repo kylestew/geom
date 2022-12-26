8c2a087f519bee045f0b08ab50d41c7552183848
Kyle Stewart <kylestew@gmail.com>
2022-12-26 14:20:10 -0700

    bug?

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="geom",
-    version="0.0.28",
+    version="0.0.29",
     author="Kyle Stewart",
     author_email="kylestew@gmail.com",
     packages=find_packages(),
