81d581fafcfcebfee60d9339ad697f39c6b4051f
Kyle Stewart <kylestew@gmail.com>
2022-12-30 22:53:13 -0700

    ...

--- geom/ops/poisson_sample_in.py
+++ geom/ops/poisson_sample_in.py
@@ -1,34 +1,26 @@
-import math
-
 from geom.ops.bounds import bounds
-from geom.ops.area import area
 from geom.ops.point_inside import point_inside
 
 from geom.internal.poison import PoissonDisc
 
 
-def poisson_sample_in(dat, density=0.9):
+def poisson_sample_in(dat, r=0.1):
     """
+    ** CAREFUL: this can take a lot of time to return if r is small **
     Scatters points in the bounds of a given shape using Poisson sampling
 
     (`dat` must implement: `bounds`, `point_inside`)
 
-    - density: density of sampling (behaves weird when using Poisson)
-    - poisson: whether or not to use Poisson sampling
+    - r: radius around each point (determines density)
 
     returns: 0 or more points scattered into the area of the bounds
     note: returns no points if the shape bounds cannot be determined
     """
-
     # found a rectangular boundary covering poly area
     b = bounds(dat)
     if b == None:
         return []
     x, y, w, h = b
-
-    # determine density setting for poisson sampling
-    density = min(density, 0.99)
-    r = math.sqrt(area(dat)) * (1.0 - density)
 
     # sample points in rect boundary
     disc = PoissonDisc(width=w, height=h, r=r)
