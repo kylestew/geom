7a069da00f0896dfe6556cfd3ab4ca0858b345e1
Kyle Stewart <kylestew@gmail.com>
2023-01-01 17:51:31 -0700

    ...

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="geom",
-    version="0.0.49",
+    version="0.0.50",
     author="Kyle Stewart",
     author_email="kylestew@gmail.com",
     packages=find_packages(),
