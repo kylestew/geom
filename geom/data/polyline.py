b4c521c64c2b89e956a52afdf1633a1898e7321a
Kyle Stewart <kylestew@gmail.com>
2023-01-01 17:54:08 -0700

    Dumb

--- geom/data/polyline.py
+++ geom/data/polyline.py
@@ -1,5 +1,9 @@
 from geom.api.apc import APC
 
+import numpy as np
+
+from geom.data.line import Line
+
 
 class Polyline(APC):
     def __init__(self, pts):
@@ -9,3 +13,13 @@ class Polyline(APC):
         return "Polyline({0})".format(self.points)
 
     # === Specialized ===
+    def lines(self):
+        pts = self.points
+        pairs = np.stack((pts, np.roll(pts, -1, axis=0)), axis=1)[0:-1]
+
+        lns = []
+        for pair in pairs:
+            a, b = pair
+            lns.append(Line(a, b))
+
+        return lns
