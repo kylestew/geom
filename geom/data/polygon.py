c85f442a9d51f223a6bc5aab4340ef5df39e7d53
Kyle Stewart <kylestew@gmail.com>
2023-05-05 12:51:48 -0600

    Spit out lines from polygons

--- geom/data/polygon.py
+++ geom/data/polygon.py
@@ -1,5 +1,9 @@
 from geom.api.apc import APC
 
+import numpy as np
+
+from geom.data.line import Line
+
 
 class Polygon(APC):
     def __init__(self, pts):
@@ -25,3 +29,14 @@ class Polygon(APC):
             Polygon((a, b, d)),
             Polygon((c, d, b)),
         ]
+
+    def lines(self):
+        pts = self.points
+        pairs = np.stack((pts, np.roll(pts, -1, axis=0)), axis=1)
+
+        lns = []
+        for pair in pairs:
+            a, b = pair
+            lns.append(Line(a, b))
+
+        return lns
