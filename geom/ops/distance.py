a116a884856fb213e73e2ff71f2419e3b358f5b2
Kyle Stewart <kylestew@gmail.com>
2023-05-11 12:08:17 -0600

    ...

--- geom/ops/distance.py
+++ geom/ops/distance.py
@@ -2,15 +2,20 @@ from math import sqrt
 
 from geom.ops.centroid import centroid
 
+from geom.data.point import Point
 
 def distance(dat, pt):
     """
     Determines the distance between a given 'pt' and geom data object
 
     - dat: data.Geom object
-    - pt: naked point array
+    - pt: naked point array or Point object
     """
     x1, y1 = centroid(dat)
-    x2, y2 = pt
+
+    if isinstance(dat, Point):
+        x2, y2 = pt.pt
+    else:
+        x2, y2 = pt
 
     return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="geom",
-    version="0.0.60",
+    version="0.0.61",
     author="Kyle Stewart",
     author_email="kylestew@gmail.com",
     packages=find_packages(),
